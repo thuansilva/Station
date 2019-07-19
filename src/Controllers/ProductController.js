50fa2f7a2e1d58e72c3220a6440cb33beb17273e
Thuan Matheus <thuansilva14@gmail.com>
2019-07-19 13:25:54 -0400

    Show store

--- src/Controllers/ProductController.js
+++ src/Controllers/ProductController.js
@@ -7,7 +7,15 @@ module.exports = {
     const product = await Product.find();
     return res.json(product);
   },
-  async store(req,res){
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+    return res.json(product);
+  },
 
-  }
+  async store(req, res) {
+    const product = await Product.create(req.body);
+    return res.json(product);
+  },
+  async update(req, res) {},
+  async destroy(req, res) {}
 };
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const routes = express.Router();
 const ProductControler = require("./Controllers/ProductController");
-routes.get("/products", ProductControler.index);
-routes.post("/products", ProductControler.store);
+routes.get("/products", ProductControler.index); //buscar
+routes.get("/products/:id", ProductControler.show); //mostrar um unico dado
+routes.post("/products", ProductControler.store); //inserir dados
 module.exports = routes;
